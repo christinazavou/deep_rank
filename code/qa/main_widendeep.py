49373b6194d886bcea16666f6b0e870f75f9602f
christinazavou <czavou01@gmail.com>
2017-08-25 16:46:16 +0200

    done for widendeep as well

--- code/qa/main_widendeep.py
+++ code/qa/main_widendeep.py
@@ -1,375 +1,19 @@
-import argparse
-import myio
-import sys
-from utils import load_embedding_iterator
-import time
-import tensorflow as tf
 import numpy as np
-import os
-from evaluation import Evaluation
-from nn import get_activation_by_name
-import gzip
-import pickle
-from prettytable import PrettyTable
-from myio import say
 import scipy.sparse
 from nltk.util import ngrams
+import argparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 import random
 from sklearn.metrics.pairwise import cosine_similarity
-from nn.initialization import random_init
+import myio
+import os
+import time
+import sys
+from utils import load_embedding_iterator
 
 
-class Model(object):
-
-    def __init__(self, args, embedding_layer):
-        self.args = args
-        self.embeddings = embedding_layer.embeddings
-        self.padding_id = embedding_layer.vocab_map["<padding>"]
-        self._initialize_graph()
-
-    def _initialize_graph(self):
-
-        with tf.name_scope('DNN_COMPONENT'):
-
-            with tf.name_scope('input'):
-                self.titles_words_ids_placeholder = tf.placeholder(tf.int32, [None, None], name='titles_ids')
-                self.bodies_words_ids_placeholder = tf.placeholder(tf.int32, [None, None], name='bodies_ids')
-                self.pairs_ids_placeholder = tf.placeholder(tf.int32, [None, None], name='bodies_ids')  # LENGTH = 3 OR 22
-
-                self.dropout_prob = tf.placeholder(tf.float32, name='dropout_prob')
-
-            with tf.name_scope('embeddings'):
-                self.titles = tf.nn.embedding_lookup(self.embeddings, self.titles_words_ids_placeholder)
-                self.titles = tf.nn.dropout(self.titles, 1.0 - self.dropout_prob)
-
-                self.bodies = tf.nn.embedding_lookup(self.embeddings, self.bodies_words_ids_placeholder)
-                self.bodies = tf.nn.dropout(self.bodies, 1.0 - self.dropout_prob)
-
-            with tf.name_scope('LSTM'):
-
-                def lstm_cell():
-                    _cell = tf.nn.rnn_cell.LSTMCell(
-                        self.args.hidden_dim, state_is_tuple=True, activation=get_activation_by_name(self.args.activation)
-                    )
-                    # _cell = tf.nn.rnn_cell.DropoutWrapper(_cell, output_keep_prob=0.5)
-                    return _cell
-
-                cell = tf.nn.rnn_cell.MultiRNNCell(
-                    [lstm_cell() for _ in range(self.args.depth)]
-                )
-
-            with tf.name_scope('titles_output'):
-                self.t_states_series, self.t_current_state = tf.nn.dynamic_rnn(
-                    cell,
-                    self.titles,
-                    dtype=tf.float32
-                )
-                # current_state = last state of every layer in the network as an LSTMStateTuple
-
-                if self.args.normalize:
-                    self.t_states_series = self.normalize_3d(self.t_states_series)
-
-                if self.args.average:
-                    self.t_state = self.average_without_padding(self.t_states_series, self.titles_words_ids_placeholder)
-                else:
-                    self.t_state = self.t_states_series[:, -1, :]
-                    # SAME AS self.t_current_state[-1][1]
-                    # SAME AS self.t_current_state[0][1]
-
-            with tf.name_scope('bodies_output'):
-                self.b_states_series, self.b_current_state = tf.nn.dynamic_rnn(
-                    cell,
-                    self.bodies,
-                    dtype=tf.float32
-                )
-                # current_state = last state of every layer in the network as an LSTMStateTuple
-
-                if self.args.normalize:
-                    self.b_states_series = self.normalize_3d(self.b_states_series)
-
-                if self.args.average:
-                    self.b_state = self.average_without_padding(self.b_states_series, self.bodies_words_ids_placeholder)
-                else:
-                    self.b_state = self.b_states_series[:, -1, :]
-                    # SAME AS self.b_current_state[-1][1]
-                    # SAME AS self.b_current_state[0][1]
-
-            with tf.name_scope('outputs'):
-                # batch * d
-                h_final = (self.t_state + self.b_state) * 0.5
-                h_final = tf.nn.dropout(h_final, 1.0 - self.dropout_prob)
-                self.h_final = self.normalize_2d(h_final)
-
-            with tf.name_scope('dnn-logits'):
-                # For testing:
-                #   first one in batch is query, the rest are candidate questions
-                self.dnn_scores = tf.reduce_sum(tf.multiply(self.h_final[0], self.h_final[1:]), axis=1)
-
-                # For training:
-                pairs_vecs = tf.nn.embedding_lookup(self.h_final, self.pairs_ids_placeholder, name='pairs_vecs')
-                # num query * n_d
-                query_vecs = pairs_vecs[:, 0, :]
-                # num query *num candidates
-                self.dnn_logits = tf.reduce_sum(tf.expand_dims(query_vecs, axis=1) * pairs_vecs[:, 1:, :], axis=2)
-
-        dnn_vars_to_train = set(tf.trainable_variables())
-
-        with tf.name_scope('WIDE-COMPONENT'):
-
-            num_features = 2
-
-            with tf.name_scope("input"):
-                # batch size * candidates * features
-                self.features_placeholder = tf.placeholder(
-                    tf.float32, [None, None, num_features], name='features'
-                )
-
-            with tf.name_scope("weights"):
-                self.W = tf.Variable(random_init([num_features, 1]), name="weight")
-                self.b = tf.Variable(np.random.random(), name="bias")
-
-            with tf.name_scope('outputs'):
-                # Construct a linear model
-                # one prediction for each sample in the batch and each candidate
-                predictions = tf.matmul(tf.reshape(self.features_placeholder, [-1, num_features]), self.W)
-
-            with tf.name_scope('linear-logits'):
-                # For testing:
-                #   first one in batch is query, the rest are candidate questions
-                self.linear_scores = tf.add(predictions, self.b)
-
-                self.linear_logits = self.linear_scores
-                self.linear_logits = tf.reshape(self.linear_logits, [-1, 21])
-
-        linear_vars_to_train = set(tf.trainable_variables()) - dnn_vars_to_train
-
-        self.dnn_vars_to_train, self.linear_vars_to_train = list(dnn_vars_to_train), list(linear_vars_to_train)
-
-        # for testing
-        with tf.name_scope('WND-scores'):
-            # in training we have one query, one positive and 20 negative
-            # in testing we have one query, 20 candidates and one dummy
-            self.scores = self.dnn_scores + tf.squeeze(self.linear_scores[:-1])
-
-        with tf.name_scope('WND-logits'):
-            self.logits = self.dnn_logits + self.linear_logits
-            self.logits = self.normalize_2d(self.logits)  # todo: keep it? i think it does change anythin
-
-        with tf.name_scope('cost'):
-
-            pos_scores = self.logits[:, 0]
-            neg_scores = self.logits[:, 1:]
-            neg_scores = tf.reduce_max(neg_scores, axis=1)
-
-            with tf.name_scope('loss'):
-
-                delta = 1.0
-
-                diff = neg_scores - pos_scores + delta
-                loss = tf.reduce_mean(tf.cast((diff > 0), tf.float32) * diff)
-                self.loss = loss
-
-            with tf.name_scope('regularization'):
-                l2_reg = 0.
-                for param in self.dnn_vars_to_train + self.linear_vars_to_train:
-                    l2_reg += tf.nn.l2_loss(param) * self.args.l2_reg
-                self.l2_reg = l2_reg
-
-            self.cost = self.loss + self.l2_reg
-
-    @staticmethod
-    def normalize_2d(x, eps=1e-8):
-        # x is batch*hid_dim
-        # l2 is batch*1
-        l2 = tf.norm(x, ord=2, axis=1, keep_dims=True)
-        return x / (l2 + eps)
-
-    @staticmethod
-    def normalize_3d(x, eps=1e-8):
-        # x is len*batch*hid_dim
-        # l2 is len*batch*1
-        l2 = tf.norm(x, ord=2, axis=2, keep_dims=True)
-        return x / (l2 + eps)
-
-    def average_without_padding(self, x, ids, eps=1e-8):
-        # len*batch*1
-        mask = tf.not_equal(ids, self.padding_id)
-        mask = tf.expand_dims(mask, 2)
-        mask = tf.cast(mask, tf.float32)
-        # batch*d
-        s = tf.reduce_sum(x*mask, axis=1) / (tf.reduce_sum(mask, axis=1)+eps)
-        return s
-
-    def evaluate(self, data, sess):
-        res = []
-
-        def eval_batch(titles, bodies, labels, features):
-            extended_features = np.zeros((21, features.shape[1]))
-            extended_features[0:-1] = features  # we add a dummy instance at the end
-
-            _scores = sess.run(
-                self.scores,
-                feed_dict={
-                    self.titles_words_ids_placeholder: titles.T,  # IT IS TRANSPOSE ;)
-                    self.bodies_words_ids_placeholder: bodies.T,  # IT IS TRANSPOSE ;)
-                    self.pairs_ids_placeholder: np.reshape(labels, (1, labels.shape[0])),
-                    self.dropout_prob: 0.,
-                    self.features_placeholder: np.reshape(extended_features, (1, 21, features.shape[1]))  # batch size 1
-                }
-            )
-
-            return _scores
-
-        for idts, idbs, id_labels, batch_features in data:
-            cur_scores = eval_batch(idts, idbs, id_labels, batch_features)
-            assert len(id_labels) == len(cur_scores)
-            ranks = (-cur_scores).argsort()
-            ranked_labels = id_labels[ranks]
-            res.append(ranked_labels)
-
-        e = Evaluation(res)
-        MAP = round(e.MAP(), 4)
-        MRR = round(e.MRR(), 4)
-        P1 = round(e.Precision(1), 4)
-        P5 = round(e.Precision(5), 4)
-        return MAP, MRR, P1, P5
-
-    def train_model(self, train_batches, dev=None, test=None, assign_ops=None):
-        with tf.Session() as sess:
-
-            result_table = PrettyTable(
-                ["Epoch", "dev MAP", "dev MRR", "dev P@1", "dev P@5", "tst MAP", "tst MRR", "tst P@1", "tst P@5"]
-            )
-            dev_MAP = dev_MRR = dev_P1 = dev_P5 = 0
-            test_MAP = test_MRR = test_P1 = test_P5 = 0
-            best_dev = -1
-
-            # Define Training procedure
-            global_step = tf.Variable(0, name="global_step", trainable=False)
-            optimizer = tf.train.AdamOptimizer(args.learning_rate)
-            grads_and_vars = optimizer.compute_gradients(self.cost)
-            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-
-            sess.run(tf.global_variables_initializer())
-
-            if assign_ops:
-                print 'assigning trained values ...\n'
-                sess.run(assign_ops)
-
-            if args.save_dir != "":
-                print("Writing to {}\n".format(args.save_dir))
-
-            # Summaries for loss and cost
-            loss_summary = tf.summary.scalar("loss", self.loss)
-            cost_summary = tf.summary.scalar("cost", self.cost)
-
-            # Train Summaries
-            train_summary_op = tf.summary.merge([loss_summary, cost_summary])
-            train_summary_dir = os.path.join(args.save_dir, "summaries", "train")
-            train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
-
-            if args.save_dir != "":
-                checkpoint_dir = os.path.join(args.save_dir, "checkpoints")
-                checkpoint_prefix = os.path.join(checkpoint_dir, "model")
-                if not os.path.exists(checkpoint_dir):
-                    os.makedirs(checkpoint_dir)
-
-            unchanged = 0
-            max_epoch = args.max_epoch
-            for epoch in xrange(max_epoch):
-                unchanged += 1
-                if unchanged > 15:
-                    break
-
-                N = len(train_batches)
-
-                train_loss = 0.0
-                train_cost = 0.0
-
-                def train_batch(titles, bodies, pairs, features):
-
-                    _, _step, _loss, _cost, _summary = sess.run(
-                        [train_op, global_step, self.loss, self.cost, train_summary_op],
-                        feed_dict={
-                            self.titles_words_ids_placeholder: titles.T,  # IT IS TRANSPOSE ;)
-                            self.bodies_words_ids_placeholder: bodies.T,  # IT IS TRANSPOSE ;)
-                            self.pairs_ids_placeholder: pairs,
-                            self.dropout_prob: np.float32(args.dropout),
-                            self.features_placeholder: features
-                        }
-                    )
-
-                    train_summary_writer.add_summary(_summary, _step)
-                    return _step, _loss, _cost
-
-                for i in xrange(N):
-                    idts, idbs, idps, batch_features = train_batches[i]
-                    cur_step, cur_loss, cur_cost = train_batch(idts, idbs, idps, batch_features)
-
-                    train_loss += cur_loss
-                    train_cost += cur_cost
-
-                    if i % 10 == 0:
-                        myio.say("\r{}/{}".format(i, N))
-
-                    if i == N-1:  # EVAL
-                        if dev:
-                            dev_MAP, dev_MRR, dev_P1, dev_P5 = self.evaluate(dev, sess)
-                        if test:
-                            test_MAP, test_MRR, test_P1, test_P5 = self.evaluate(test, sess)
-
-                        if dev_MRR > best_dev:
-                            unchanged = 0
-                            best_dev = dev_MRR
-                            result_table.add_row(
-                                [epoch, dev_MAP, dev_MRR, dev_P1, dev_P5, test_MAP, test_MRR, test_P1, test_P5]
-                            )
-                            if args.save_dir != "":
-                                self.save(sess, checkpoint_prefix, cur_step)
-
-                        say("\r\n\nEpoch {}\tcost={:.3f}\tloss={:.3f}\tMRR={:.2f},{:.2f}\n".format(
-                            epoch,
-                            train_cost / (i+1),  # i.e. divided by N training batches
-                            train_loss / (i+1),  # i.e. divided by N training batches
-                            dev_MRR,
-                            best_dev
-                        ))
-                        say("\n{}\n".format(result_table))
-
-    def save(self, sess, path, step):
-        path = "{}_{}_{}".format(path, step, ".pkl.gz")
-        print("Saving model checkpoint to {}\n".format(path))
-        params = tf.trainable_variables()  # todo: self.dnn_vars + self.linear_vars
-        params_dict = {}
-        for param in params:
-            params_dict[param.name] = sess.run(param)
-        # print 'params_dict\n', params_dict
-        with gzip.open(path, "w") as fout:
-            pickle.dump(
-                {
-                    "params_dict": params_dict,
-                    "args": self.args,
-                    "step": step
-                },
-                fout,
-                protocol=pickle.HIGHEST_PROTOCOL
-            )
-
-    def load_trained_vars(self, path):
-        print("Loading model checkpoint from {}\n".format(path))
-        assign_ops = []
-        with gzip.open(path) as fin:
-            data = pickle.load(fin)
-            assert self.args == data['args'], 'some different args:\n{}'.format(self.args)
-            params_dict = data['params_dict']
-            graph = tf.get_default_graph()
-            for param_name, param_value in params_dict.iteritems():
-                variable = graph.get_tensor_by_name(param_name)
-                assign_op = tf.assign(variable, param_value)
-                assign_ops.append(assign_op)
-        return assign_ops
+from main_model_widendeep import Model
 
 
 def make_features_dicts(ids_corpus, raw_corpus):
@@ -433,8 +77,8 @@ def create_eval_batches(ids_corpus, data, padding_id, f1_vectors, f2_vectors, pa
 
         if i % 100 == 0:
             print 'i is ', i
-        # if i == 10:  # TEST
-        #     break
+        if i == 10:  # TEST
+            break
 
         titles = []
         bodies = []
@@ -491,7 +135,7 @@ def create_batches(ids_corpus, data, batch_size, padding_id, f1_vectors, f2_vect
     for u in xrange(N):
         if u % 100 == 0:
             print 'u is ', u
-        if u == 15000:  # TAKES TOO LONG..TEST  8000
+        if u == 100:  # TAKES TOO LONG..RUN ON  15000
             break
         i = perm[u]
         pid, qids, qlabels = data[i]
@@ -571,6 +215,7 @@ def main(args):
         test = create_eval_batches(ids_corpus, test, padding_id, n_gram_vectors, pos_tag_n_gram_vectors, pad_left=not args.average)
 
     model = Model(args, embedding_layer)
+    model.ready()
 
     assign_ops = model.load_trained_vars(args.load_pretrain) if args.load_pretrain else None
 
