cec6e95f283e1405c86cf1cc22d43d6b4f09dda9
christinazavou <czavou01@gmail.com>
2017-08-25 16:35:07 +0200

    main model and inheritance models

--- code/qa/api.py
+++ code/qa/api.py
@@ -1,115 +1,102 @@
 import argparse
-import json
 import myio
 import sys
 from myio import say
-from main import Model
-import os
-from utils import load_embedding_iterator, read_eval_rows, read_questions
-import tensorflow as tf
+from utils import load_embedding_iterator
 import numpy as np
-import gzip
-import pickle
 from evaluation import Evaluation
+import tensorflow as tf
 
-
-def load_model(model_path, corpus_path, emb_path):
-    print("Loading model checkpoint from {}\n".format(model_path))
-
-    assign_ops = []
-
-    raw_corpus = myio.read_corpus(corpus_path)
-
-    with gzip.open(model_path) as fin:
-        data = pickle.load(fin)
-        model_args = data['args']
-
-        embedding_layer = myio.create_embedding_layer(
-            raw_corpus,
-            n_d=model_args.hidden_dim,
-            cut_off=model_args.cut_off,
-            embs=load_embedding_iterator(emb_path)
-        )
-        say("vocab size={}, corpus size={}\n".format(
-            embedding_layer.n_V,
-            len(raw_corpus)
-        ))
-
-        model = Model(args=model_args, embedding_layer=embedding_layer)
-
-        params_dict = data['params_dict']
-        graph = tf.get_default_graph()
-        for param_name, param_value in params_dict.iteritems():
-            variable = graph.get_tensor_by_name(param_name)
-            assign_op = tf.assign(variable, param_value)
-            assign_ops.append(assign_op)
-
-    return assign_ops, model, embedding_layer
+# todo: ajust
+# from main_model import Model
+# from main_model_nostate import Model
+# from main_model_1layer import Model
+from main_model_bidirectional import Model
 
 
 class QRAPI:
 
-    def __init__(self, model_path, corpus_path, emb_path):
-        self.assign_ops, self.model, self.embedding_layer = load_model(model_path, corpus_path, emb_path)
+    def __init__(self, model_path, corpus_path, emb_path, session):
+        raw_corpus = myio.read_corpus(corpus_path)
+        embedding_layer = myio.create_embedding_layer(
+                    raw_corpus,
+                    n_d = 10,
+                    cut_off = 1,
+                    embs = load_embedding_iterator(emb_path)
+                )
+        # weights = myio.create_idf_weights(corpus_path, embedding_layer)
+        say("vocab size={}, corpus size={}\n".format(
+                embedding_layer.n_V,
+                len(raw_corpus)
+            ))
 
-    def initialize(self, sess):
-        sess.run(tf.global_variables_initializer())
-        sess.run(self.assign_ops)
+        # model = Model(args=None, embedding_layer=embedding_layer,
+        #             weights=weights)
+        model = Model(args=None, embedding_layer=embedding_layer)
 
-    def rank(self, sess, query):
+        model.load_n_set_model(model_path, session)
+        say("model initialized\n")
 
-        def score_batch(titles, bodies):
-            _current_state = np.zeros((self.model.args.depth, 2, titles.T.shape[0], self.model.args.hidden_dim))
-            _scores = sess.run(
+        self.model = model
+
+        def score_func(titles, bodies, cur_sess):
+            _scores = cur_sess.run(
                 self.model.scores,
                 feed_dict={
                     self.model.titles_words_ids_placeholder: titles.T,  # IT IS TRANSPOSE ;)
                     self.model.bodies_words_ids_placeholder: bodies.T,  # IT IS TRANSPOSE ;)
-                    self.model.pairs_ids_placeholder: np.zeros((1, 100)),  # will be unused
                     self.model.dropout_prob: 0.,
-                    self.model.init_state: _current_state
+
+                    # todo: ajust
+                    # self.model.init_state: np.zeros((self.model.args.depth, 2, titles.T.shape[0], self.model.args.hidden_dim))
+
                 }
             )
             return _scores
+        self.score_func = score_func
+        say("scoring function compiled\n")
 
-        if isinstance(query, str) or isinstance(query, unicode):
-            query = json.loads(query)
+    def evaluate(self, data, session):
 
-        p_t, p_b = query["query"][0].strip().split(), query["query"][1].strip().split()
+        eval_func = self.score_func
+        res_ranked_labels = []
+        res_ranked_ids = []
+        res_ranked_scores = []
+        query_ids = []
+        all_MAP, all_MRR, all_Pat1, all_Pat5 = [], [], [], []
+        for idts, idbs, labels, pid, qids in data:
+            scores = eval_func(idts, idbs, session)
+            assert len(scores) == len(labels)
+            ranks = (-scores).argsort()
+            ranked_scores = np.array(scores)[ranks]
+            ranked_labels = labels[ranks]
+            ranked_ids = np.array(qids)[ranks]
+            query_ids.append(pid)
+            res_ranked_labels.append(ranked_labels)
+            res_ranked_ids.append(ranked_ids)
+            res_ranked_scores.append(ranked_scores)
+            this_ev = Evaluation([ranked_labels])
+            all_MAP.append(this_ev.MAP())
+            all_MRR.append(this_ev.MRR())
+            all_Pat1.append(this_ev.Precision(1))
+            all_Pat5.append(this_ev.Precision(5))
 
-        questions_t = [self.embedding_layer.map_to_ids(p_t, filter_oov=True)]
-        questions_b = [self.embedding_layer.map_to_ids(p_b, filter_oov=True)]
+        print 'average all ... ', sum(all_MAP)/len(all_MAP), sum(all_MRR)/len(all_MRR), sum(all_Pat1)/len(all_Pat1), sum(all_Pat5)/len(all_Pat5)
+        return all_MAP, all_MRR, all_Pat1, all_Pat5, res_ranked_labels, res_ranked_ids, query_ids, res_ranked_scores
 
-        for q in query["candidates"]:
-            q_t, q_b = q[0].strip().split(), q[1].strip().split()
 
-            questions_t.append(
-                self.embedding_layer.map_to_ids(q_t, filter_oov=True)
-            )
-            questions_b.append(
-                self.embedding_layer.map_to_ids(q_b, filter_oov=True)
-            )
-
-        batch_titles, batch_bodies = myio.create_one_batch(
-            questions_t,
-            questions_b,
-            self.embedding_layer.vocab_map["<padding>"],
-            not self.model.args.average
-        )
-
-        scores = score_batch(batch_titles, batch_bodies)
-
-        assert len(scores) == batch_titles.shape[1] - 1, ' error in scores shape '
-
-        # if ("BM25" in query) and ("ratio" in query):
-        #     BM25 = query["BM25"]
-        #     ratio = query["ratio"]
-        #     assert len(BM25) == len(scores)
-        #     assert ratio >= 0 and ratio <= 1.0
-        #     scores = [ x*(1-ratio)+y*ratio for x,y in zip(scores, BM25) ]
-
-        ranks = sorted(range(len(scores)), key=lambda i: -scores[i])
-        return {"ranks": ranks, "scores": scores}
+def create_eval_batches(ids_corpus, data, padding_id, pad_left):
+    lst = []
+    for pid, qids, qlabels in data:
+        titles = []
+        bodies = []
+        for id in [pid]+qids:
+            t, b = ids_corpus[id]
+            titles.append(t)
+            bodies.append(b)
+        titles, bodies = myio.create_one_batch(titles, bodies, padding_id, pad_left)
+        lst.append((titles, bodies, np.array(qlabels, dtype="int32"), pid, qids))
+    return lst
 
 
 if __name__ == '__main__':
@@ -118,57 +105,36 @@ if __name__ == '__main__':
     argparser.add_argument("--corpus_path", type=str, default="")
     argparser.add_argument("--emb_path", type=str, default="")
     argparser.add_argument("--dev", type=str, default="")
-    argparser.add_argument("--results_path", type=str, default="")
     argparser.add_argument("--results_file", type=str, default="")
     args = argparser.parse_args()
     print '\n', args, '\n'
 
-    myqrapi = QRAPI(args.model_path, args.corpus_path, args.emb_path)
+    with tf.Session() as sess:
 
-    Q = list(read_questions(args.corpus_path))
-    E = list(read_eval_rows(args.dev))
+        myqrapi = QRAPI(args.model_path, args.corpus_path, args.emb_path, sess)
 
-    if not os.path.exists(args.results_path):
-        os.makedirs(args.results_path)
+        raw_corpus = myio.read_corpus(args.corpus_path)
+        embedding_layer = myqrapi.model.embedding_layer
+        ids_corpus = myio.map_corpus(raw_corpus, embedding_layer, max_len=100)
+        dev = myio.read_annotations(args.dev, K_neg=-1, prune_pos_cnt=-1)
+        dev = create_eval_batches(ids_corpus, dev, myqrapi.model.padding_id, pad_left=not myqrapi.model.args.average)
 
-    def label(x, y):
-        return 1 if x in y else 0
+        devmap, devmrr, devpat1, devpat5, rank_labels, rank_ids, qids, rank_scores = myqrapi.evaluate(dev, sess)
 
-    MAP, MRR, Pat1, Pat5 = [], [], [], []
-    with open(os.path.join(args.results_path, args.results_file), 'w') as f:
+        with open(args.results_file, 'w') as f:
+            for i, (_, _, labels, pid, qids) in enumerate(dev):
+                print_qids_similar = [x for x, l in zip(qids, labels) if l == 1]
+                print_qids_similar = " ".join([str(x) for x in print_qids_similar])
 
-        with tf.Session() as session:
-            myqrapi.initialize(session)
+                print_qids_candidates = " ".join([str(x) for x in rank_ids[i]])
 
-            Qdict = {id: (qt, qb) for id, qt, qb in Q}
-            for qid, qids_similar, qids_candidates in E:
-                query_ = Qdict[qid]
-                candidates = [Qdict[idc] for idc in qids_candidates]
+                print_ranked_scores = " ".join([str(x) for x in rank_scores[i]])
 
-                results = myqrapi.rank(session, {"query": query_, "candidates": candidates})
+                print_ranked_labels = " ".join([str(x) for x in rank_labels[i]])
 
-                ranked = results['ranks']
-                scores = results['scores']
-                scores = sorted(scores, reverse=True)
-
-                ranked_candidates = np.array(qids_candidates)[ranked]
-                labels = [label(item, qids_similar) for item in ranked_candidates]
-                ev = Evaluation([labels])
-                map_, mrr_, pat1_, pat5_ = ev.MAP(), ev.MRR(), ev.Precision(1), ev.Precision(5)
-                MAP.append(map_), MRR.append(mrr_), Pat1.append(pat1_), Pat5.append(pat5_)
-
-                qids_candidates = list(np.array(qids_candidates)[ranked])
-                qids_similar = " ".join([str(x) for x in qids_similar])
-                qids_candidates = " ".join([str(x) for x in qids_candidates])
-                scores = " ".join([str(x) for x in scores])
-                labels = " ".join([str(x) for x in labels])
                 f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-                    qid, qids_similar, qids_candidates,
-                    scores,
-                    labels,
-                    map_, mrr_, pat1_, pat5_
+                    pid, print_qids_similar, print_qids_candidates,
+                    print_ranked_scores,
+                    print_ranked_labels,
+                    round(devmap[i],4), round(devmrr[i],4), round(devpat1[i],4), round(devpat5[i],4)
                 ))
-    print 'final mean MAP\tMRR\tPat1\tPat5:\n           {}\t{}\t{}\t{}\n'.format(
-        sum(MAP)/len(MAP), sum(MRR)/len(MRR), sum(Pat1)/len(Pat1), sum(Pat5)/len(Pat5)
-    )
-
