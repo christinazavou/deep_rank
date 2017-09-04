c88296f8b716be360124cd6b7a3710ed7bf0e389
christinazavou <czavou01@gmail.com>
2017-09-04 20:57:50 +0200

    sklearn ok

--- code/tags_prediction/tags_classification_sklearn.py
+++ code/tags_prediction/tags_classification_sklearn.py
@@ -5,7 +5,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 # from sklearn.metrics import classification_report
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 import pickle
 import sys
@@ -48,7 +48,7 @@ def grid_search(train_x, train_y, dev_x, dev_y, parameters, pipeline):
     ty = np.vstack((train_y, dev_y))
     test_fold = [0 for _ in train_x] + [-1 for _ in dev_x]
     ps = PredefinedSplit(test_fold)
-    grid_search_tune = GridSearchCV(pipeline, parameters, n_jobs=3, verbose=10, cv=ps)
+    grid_search_tune = GridSearchCV(pipeline, parameters, n_jobs=args.njobs, verbose=10, cv=ps)
     grid_search_tune.fit(tx, ty)
 
     print
@@ -61,6 +61,13 @@ def grid_search(train_x, train_y, dev_x, dev_y, parameters, pipeline):
     best_clf = grid_search_tune.best_estimator_
 
     return best_clf
+
+
+def model_fit(train_x, train_y, dev_x, dev_y, pipeline):
+    tx = np.concatenate((train_x, dev_x))
+    ty = np.vstack((train_y, dev_y))
+    pipeline.fit(tx, ty)
+    return pipeline
 
 
 def evaluate(test_x, test_y, labels, model):
@@ -91,9 +98,7 @@ def load_model(filename):
     return pickle.load(open(filename, 'rb'))
 
 
-def main(args):
-
-    TEST = True
+def main():
 
     df = read_df(args.df_path)
     df = df.fillna(u'')
@@ -105,47 +110,69 @@ def main(args):
     x_dev, y_dev = dev_data
     x_test, y_test = test_data
 
-    if TEST:
+    if args.test:
         x_train, y_train = x_train[0:100], y_train[0:100]
         x_dev, y_dev = x_dev[0:100], y_dev[0:100]
         x_test, y_test = x_test[0:100], y_test[0:100]
 
     if args.method == 'logreg':
-        clf = OneVsRestClassifier(LogisticRegression(solver='sag'), n_jobs=1)
-        parameters = {
-            'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
-            "clf__estimator__C": [0.01, 0.1, 1],
-            "clf__estimator__class_weight": ['balanced', None],
-        }
+        clf = OneVsRestClassifier(LogisticRegression(solver='sag', verbose=10), n_jobs=1)
+        if args.predefined:
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df, ngram_range=(1, args.n_grams))
+        else:
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df)
+            parameters = {
+                'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
+                "clf__estimator__C": [0.01, 0.1, 1],
+                "clf__estimator__class_weight": ['balanced', None],
+            }
     elif args.method == 'svm':
-        clf = OneVsRestClassifier(LinearSVC(), n_jobs=1)
-        parameters = {
-            'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
-            "clf__estimator__C": [0.01, 0.1, 1],
-            "clf__estimator__class_weight": ['balanced', None],
-        }
-    elif args.method == 'dtree':
-        clf = OneVsRestClassifier(DecisionTreeClassifier(), n_jobs=1)
-        parameters = {
-            'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
-            "clf__estimator__criterion": ["gini", "entropy"],
-            "clf__estimator__class_weight": ['balanced', None],
-        }
+        clf = OneVsRestClassifier(LinearSVC(verbose=10), n_jobs=1)
+        if args.predefined:
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df, ngram_range=(1, args.n_grams))
+        else:
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df)
+            parameters = {
+                'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
+                "clf__estimator__C": [0.01, 0.1, 1],
+                "clf__estimator__class_weight": ['balanced', None],
+            }
+    elif args.method == 'randforest':
+        if args.predefined:
+            clf = OneVsRestClassifier(RandomForestClassifier(verbose=10, criterion="entropy"), n_jobs=1)
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df, ngram_range=(1, args.n_grams))
+        else:
+            clf = OneVsRestClassifier(RandomForestClassifier(verbose=10), n_jobs=1)
+            tfidf = TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df)
+            parameters = {
+                'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
+                # "clf__estimator__criterion": ["gini", "entropy"],
+                "clf__estimator__criterion": ["entropy"],
+                # "clf__estimator__n_estimators": [10, 50, 100],
+                "clf__estimator__n_estimators": [10, 100],
+                # "clf__estimator__class_weight": ['balanced', None]
+            }
     else:
         raise Exception('unknown method')
 
     pipeline = Pipeline([
-        ('tfidf', TfidfVectorizer(min_df=args.cut_off, max_df=args.max_df)),
+        ('tfidf', tfidf),
         ('clf', clf),
     ])
 
     if os.path.isfile(args.model_file):
         tuned_model = load_model(args.model_file)
     else:
-        tuned_model = grid_search(x_train, y_train, x_dev, y_dev, parameters, pipeline)
+        if args.predefined:
+            tuned_model = model_fit(x_train, y_train, x_dev, y_dev, pipeline)
+        else:
+            tuned_model = grid_search(x_train, y_train, x_dev, y_dev, parameters, pipeline)
         save_model(tuned_model, args.model_file)
 
+    print 'EVALUATE ON TEST\n'
     evaluate(x_test, y_test, label_tags, tuned_model)
+    print 'EVALUATE ON DEV\n'
+    evaluate(x_dev, y_dev, label_tags, tuned_model)
 
 
 if __name__ == '__main__':
@@ -155,21 +182,21 @@ if __name__ == '__main__':
 
     argparser.add_argument("--cut_off", type=int, default=5)
     argparser.add_argument("--max_df", type=float, default=0.75)
+    argparser.add_argument("--n_grams", type=int, default=3)
 
     # argparser.add_argument("--max_seq_len", type=int, default=-1)
     # argparser.add_argument("--batch_size", type=int, default=40)
-    # argparser.add_argument("--learning_rate", type=float, default=0.001)
-    # argparser.add_argument("--l2_reg", type=float, default=1e-5)
-    # argparser.add_argument("--max_epoch", type=int, default=50)
-    # argparser.add_argument("--ngrams", type=int, default=1)
-    # argparser.add_argument("--out_file", type=str)
 
     argparser.add_argument("--method", type=str, default='logreg')
     argparser.add_argument("--model_file", type=str)
     argparser.add_argument("--tags_file", type=str)
 
+    argparser.add_argument("--njobs", type=int, default=2)
+    argparser.add_argument("--test", type=bool, default=False)
+    argparser.add_argument("--predefined", type=bool, default=False)
+
     args = argparser.parse_args()
     print args
     print
-    main(args)
+    main()
 
