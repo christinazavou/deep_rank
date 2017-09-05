7d339352b6331ad85a416f05045737dd73634317
christinazavou <czavou01@gmail.com>
2017-09-05 18:35:45 +0200

    separate tags models

---
+++ code/tags_prediction/main.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import pickle
+import sys
+import time
+
+import myio as myio_
+from qa import myio
+from utils import load_embedding_iterator
+from utils.statistics import read_df
+
+
+# from tags_classification_model import Model
+from tags_classification_bilstm import Model
+
+
+def main():
+    df = read_df(args.df_path)
+    df = df.fillna(u'')
+
+    label_tags = pickle.load(open(args.tags_file, 'rb'))
+
+    raw_corpus = myio_.read_corpus(args.corpus, with_tags=True)
+    embedding_layer = myio.create_embedding_layer(
+                raw_corpus,
+                n_d=240,
+                cut_off=1,
+                embs=load_embedding_iterator(args.embeddings) if args.embeddings else None
+            )
+
+    ids_corpus_tags = myio_.make_tag_labels(df, label_tags)
+
+    ids_corpus = myio_.map_corpus(raw_corpus, embedding_layer, ids_corpus_tags, max_len=args.max_seq_len)
+
+    print("vocab size={}, corpus size={}\n".format(
+            embedding_layer.n_V,
+            len(raw_corpus)
+        ))
+    padding_id = embedding_layer.vocab_map["<padding>"]
+
+    if args.reweight:
+        weights = myio_.create_idf_weights(args.corpus, embedding_layer, with_tags=True)
+
+    dev = myio_.create_batches(df, ids_corpus, 'dev', args.batch_size, padding_id, pad_left=not args.average)
+    test = myio_.create_batches(df, ids_corpus, 'test', args.batch_size, padding_id, pad_left=not args.average)
+    train = myio_.create_batches(df, ids_corpus, 'train', args.batch_size, padding_id, pad_left=not args.average)
+    print '{} batches of {} instances in dev, {} in test and {} in train.'.format(
+        len(dev), args.batch_size, len(test), len(train))
+
+    model = Model(args, embedding_layer, len(label_tags), weights=weights if args.reweight else None)
+    model.ready()
+
+    model.train_model(train, dev=dev, test=test)
+
+
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser(sys.argv[0])
+    argparser.add_argument("--corpus", type=str)
+    argparser.add_argument("--df_path", type=str)
+
+    argparser.add_argument("--embeddings", type=str, default="")
+    argparser.add_argument("--hidden_dim", "-d", type=int, default=200)
+    argparser.add_argument("--cut_off", type=int, default=1)
+    argparser.add_argument("--max_seq_len", type=int, default=100)
+
+    argparser.add_argument("--batch_size", type=int, default=40)
+    argparser.add_argument("--learning_rate", type=float, default=0.001)
+    argparser.add_argument("--l2_reg", type=float, default=1e-5)
+    argparser.add_argument("--activation", "-act", type=str, default="tanh")
+    argparser.add_argument("--dropout", type=float, default=0.0)
+    argparser.add_argument("--max_epoch", type=int, default=50)
+    argparser.add_argument("--reweight", type=int, default=1)
+    argparser.add_argument("--normalize", type=int, default=1)
+    argparser.add_argument("--average", type=int, default=0)
+    argparser.add_argument("--depth", type=int, default=1)
+
+    timestamp = str(int(time.time()))
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    out_dir = os.path.join(this_dir, "runs", timestamp)
+
+    argparser.add_argument("--save_dir", type=str, default=out_dir)
+    argparser.add_argument("--tags_file", type=str)
+
+    argparser.add_argument("--loss_type", type=str, default='xentropy')
+    argparser.add_argument("--threshold", type=float, default=0.5)
+
+    args = argparser.parse_args()
+    print args
+    print
+    main()
+
